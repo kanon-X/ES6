bb0a7730f715a4f65b2b61c5bcac61d73be412c1
kanon-X <syedur.b@uttarauniversity.edu.bd>
2026-01-25 10:14:29 +0600

    initial commit

---
+++ Template_literals.js
@@ -0,0 +1,6 @@
+const str = `Hello 'This is kanon'`
+console.log(str);
+
+const name  = `kanon`;
+const age = 22;
+console.log(`My name is ${name} & I am ${age} years old.`)
---
+++ forinloop.js
@@ -0,0 +1,9 @@
+const array = [10, 20, 30, 40, 50]
+for (value of array){
+console.log(value)
+}
+
+//element  index
+for (index in array){
+    console.log(index)
+}
